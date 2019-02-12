28b20fb8f4e4c836fc94dc1cfebeba49361d62f0
helloworld-163959robertomartinez <47367149+helloworld-163959robertomartinez@users.noreply.github.com>
2019-02-12 13:19:35 -0700

    Add files via upload

---
+++ Ejercicio5.java
@@ -0,0 +1,19 @@
+//Roberto Martinez Palacios
+//08/02/2019
+//12:09pm
+//12:23pm
+
+import java.util.Scanner;
+  public class Ejercicio5 {
+	public static void main(String[] args){
+	Scanner in = new Scanner (System.in);
+	
+	System.out.print("Primer numero");
+	int num1 = in.nextInt();
+	
+	System.out.print("Segundo numero");
+	int num2 = in.nextInt();
+	
+	System.out.println(num1 * num2);
+ }
+}
