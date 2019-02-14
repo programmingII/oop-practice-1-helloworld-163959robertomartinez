061d1c3022a932874caa64aed921c10482cb18f5
helloworld-163959robertomartinez <47367149+helloworld-163959robertomartinez@users.noreply.github.com>
2019-02-14 12:22:20 -0700

    Add files via upload

---
+++ Ejercicio9.java
@@ -0,0 +1,14 @@
+//Roberto Martinez Palacios
+//12/02/2019
+//1:24pm
+//
+
+	import java.util.Scanner;
+	 public class Ejercicio9 {
+	  public static void main(String[] args){
+	  
+	 System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+
+
+ }
+}
